2d462588625e24f86a70251245b453cda5615f67
zack1217 <zacharyjohnson@lcad.edu>
2026-03-24 21:11:13 -0700

    moved files into project based folders and changed folders to . to hide

---
+++ link.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+# this links from where the files are stored to the website
+# mkdir ~/zackdraws.github.io/anp/EVRYDY/
+#sudo ln /home/ok/.config/syncthing/2026/AA/EVRYDY/01/001/001.mp4 ~/zackdraws.github.io/anp/EVRYDY/
+#sudo ln /home/ok/.config/syncthing/2026/AA/EVRYDY/01/002/002.mp4 ~/zackdraws.github.io/anp/EVRYDY/
+#sudo ln /home/ok/.config/syncthing/2026/AA/EVRYDY/01/003/003.mp4 ~/zackdraws.github.io/anp/EVRYDY/
+#sudo ln /home/ok/.config/syncthing/2026/AA/EVRYDY/01/004/004.mp4 ~/zackdraws.github.io/anp/EVRYDY/
+#sudo ln /home/ok/.config/syncthing/2026/AA/EVRYDY/01/005/005.mp4 ~/zackdraws.github.io/anp/EVRYDY/
+#sudo ln /home/ok/.config/syncthing/2026/AA/EVRYDY/01/007/07.mp4 ~/zackdraws.github.io/anp/EVRYDY/
+#sudo ln /home/ok/.config/syncthing/2026/AA/EVRYDY/01/008/08.mp4 ~/zackdraws.github.io/anp/EVRYDY/
+#sudo ln /home/ok/.config/syncthing/2026/AA/EVRYDY/01/009/09.mp4 ~/zackdraws.github.io/anp/EVRYDY/
+#sudo ln /home/ok/.config/syncthing/2026/AA/EVRYDY/01/010/10.mp4 ~/zackdraws.github.io/anp/EVRYDY/
+#sudo ln /home/ok/.config/syncthing/2026/AA/EVRYDY/01/011/11.mp4 ~/zackdraws.github.io/anp/EVRYDY/
+#sudo ln /home/ok/.config/syncthing/2026/AA/EVRYDY/01/012/12.mp4 ~/zackdraws.github.io/anp/EVRYDY/
+#sudo ln /home/ok/.config/syncthing/2026/AA/EVRYDY/01/013/13.mp4 ~/zackdraws.github.io/anp/EVRYDY/
+#sudo ln /home/ok/.config/syncthing/2026/AA/EVRYDY/01/014/14.mp4 ~/zackdraws.github.io/anp/EVRYDY/
+#sudo ln /home/ok/.config/syncthing/2026/AA/EVRYDY/01/015/15.mp4 ~/zackdraws.github.io/anp/EVRYDY/
+#sudo ln /home/ok/.config/syncthing/2026/AA/EVRYDY/01/016/16.mp4 ~/zackdraws.github.io/anp/EVRYDY/
+#sudo ln /home/ok/.config/syncthing/2026/AA/EVRYDY/01/017/17.mp4 ~/zackdraws.github.io/anp/EVRYDY/
+#sudo ln /home/ok/.config/syncthing/2026/AA/EVRYDY/01/018/18.mp4 ~/zackdraws.github.io/anp/EVRYDY/
+#sudo ln /home/ok/.config/syncthing/2026/AA/EVRYDY/01/019/19.mp4 ~/zackdraws.github.io/anp/EVRYDY/
+sudo ln /home/ok/.config/syncthing/2026/DES/LAY/food-vendor/food-vendor.jpg ~/zackdraws.github.io/bg/bg_19.jpg
